4d723ecee3e6cdeaf2aaa83ca38c8a227864b629
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-06 17:18:44 +0000

    Update glicthandwrite.py

--- glicthandwrite.py
+++ glicthandwrite.py
@@ -100,8 +100,7 @@ def main():
         try:
             # Pick a random image
             image_path = pick_random_image(folder_path)
-            #st.image(image_path, caption="Original Image", use_column_width=True)
-            st.image(image_path, "Original Image", 300)
+            #st.image(image_path, "Original Image", 300)
             # Load the image
             image = Image.open(image_path)
             
